1178d92b11f5d7022413ca9640d557049cc477ba
Lauri__ <lartt1@windowslive.com>
2018-11-15 02:09:24 +0200

    Re-init.

--- tools/git/hooks/install_hooks.ps1
+++
@@ -1,2 +0,0 @@
-New-Item -ItemType HardLink -Name ../../../.git/hooks/pre-commit -Value src/pre_commit.py
-New-Item -ItemType HardLink -Name ../../../.git/hooks/commit-msg -Value src/commit_msg.py
--- tools/git/hooks/install_hooks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ../../../.git/hooks/*
-ln src/pre_commit.py ../../../.git/hooks/pre-commit
-ln src/commit_msg.py ../../../.git/hooks/commit-msg
